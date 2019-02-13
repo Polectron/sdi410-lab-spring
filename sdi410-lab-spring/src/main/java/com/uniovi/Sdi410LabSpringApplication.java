bfe9a831ed40e09a3a89720b9feec9187fa9179e
polectron <polectron@gmail.com>
2019-02-13 02:20:16 +0100

    Creación inicial del proyecto sdi410-lab-spring

---
+++ sdi410-lab-spring/src/main/java/com/uniovi/Sdi410LabSpringApplication.java
@@ -0,0 +1,14 @@
+package com.uniovi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sdi410LabSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sdi410LabSpringApplication.class, args);
+	}
+
+}
+
