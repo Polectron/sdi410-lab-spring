13c42ea4f8b08ebf8c9f95d423b4199cef7b3e7a
polec <polec@DESKTOP-HE5TSCV>
2019-02-27 00:33:19 +0100

    SDI-SpringBoot-Sesión-4.3-Control de acceso por roles

--- sdi410-lab-spring/src/main/java/com/uniovi/controllers/MarksController.java
+++ sdi410-lab-spring/src/main/java/com/uniovi/controllers/MarksController.java
@@ -27,7 +27,7 @@ public class MarksController {
 
 	@RequestMapping("/mark/list")
 	public String getList(Model model) {
-		
+
 		model.addAttribute("markList", marksService.getMarks());
 		return "mark/list";
 	}
@@ -76,5 +76,17 @@ public class MarksController {
 		original.setDescription(mark.getDescription());
 		marksService.addMark(original);
 		return "redirect:/mark/details/" + id;
+	}
+
+	@RequestMapping(value = "/mark/{id}/resend", method = RequestMethod.GET)
+	public String setResendTrue(Model model, @PathVariable Long id) {
+		marksService.setMarkResend(true, id);
+		return "redirect:/mark/list";
+	}
+
+	@RequestMapping(value = "/mark/{id}/noresend", method = RequestMethod.GET)
+	public String setResendFalse(Model model, @PathVariable Long id) {
+		marksService.setMarkResend(false, id);
+		return "redirect:/mark/list";
 	}
 }
